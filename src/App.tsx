ac9ffb2c6480eb36ed75396cc5891bcb80553c81
Redian Fikri <redianmf@gmail.com>
2024-07-26 14:45:14 +0700

    feat(chore): init project

---
+++ src/App.tsx
@@ -0,0 +1,5 @@
+function App() {
+  return <p>Test web3 project</p>;
+}
+
+export default App;
---
+++ vite.config.ts
@@ -0,0 +1,14 @@
+import react from "@vitejs/plugin-react-swc";
+import { defineConfig, loadEnv } from "vite";
+
+export default defineConfig(({ mode }) => {
+  // Load env file based on `mode` in the current working directory.
+  // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
+  const env = loadEnv(mode, process.cwd(), "");
+  return {
+    define: {
+      "process.env": env,
+    },
+    plugins: [react()],
+  };
+});
